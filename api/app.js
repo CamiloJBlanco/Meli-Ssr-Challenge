38184b303789f439aeeb0c244da6c9ddef27f17d
CamiloJBlanco <cblancodelavega@gmail.com>
2022-08-04 20:37:51 -0300

    starting backend

---
+++ api/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const client = "http://localhost:3000";
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `${client}`);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Allow", "GET, POST");
+  next();
+});
+
+module.exports = app;
--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./assets/css/index.scss";
 import Header from "./components/header";
 import Home from "./components/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
