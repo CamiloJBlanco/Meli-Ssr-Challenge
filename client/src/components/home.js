089b291ca777f1fd259987dd39bb6ac2f9d31afb
CamiloJBlanco <cblancodelavega@gmail.com>
2022-08-06 11:30:26 -0300

    updating frontend & loading component

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loading from './loading';
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
                     <small className="card-smalltitle">(Comience a buscar sus productos)</small>
                 </div>
             </div>
+            <Loading />
         </div>
     )
 }
---
+++ client/src/components/loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../assets/css/loading.scss';
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="card-message">
+        <div className="loading-print"></div>
+      </div>
+    </div>
+  );
+}
